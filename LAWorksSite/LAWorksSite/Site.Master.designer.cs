4dacb813cacea6dbe6fc81eb6be704e62e26e2e2
cmransonet <cmransonet@gmail.com>
2014-11-11 11:35:28 -0600

    test commit

--- LAWorksSite/LAWorksSite/Site.Master.designer.cs
+++ LAWorksSite/LAWorksSite/Site.Master.designer.cs
@@ -31,15 +31,6 @@ namespace LAWorksSite {
         protected global::System.Web.UI.WebControls.Button ID_Resume;
         
         /// <summary>
-        /// ID_Links control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button ID_Links;
-        
-        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
