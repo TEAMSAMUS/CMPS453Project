6fc3279ce5cf52b824437e0e35e8144922d238b5
ewoods2011 <edward.woods@rithmsgroup.com>
2014-11-29 17:07:31 -0600

    Updating Github files

--- LAWorksSite/LAWorksSite/UploadResume.aspx
+++ LAWorksSite/LAWorksSite/UploadResume.aspx
@@ -1,8 +1,10 @@
 ﻿<%@ Page Title="Upload Resume | Louisiana Works Career Seeker" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="UploadResume.aspx.cs" Inherits="LAWorksSite.UploadResume" %>
 <asp:Content ID="UploadContent" ContentPlaceHolderID="MainContent" runat="server">
     <div align="center">
-       
+       <b>Select a File to Upload (Word or PDF only)</b>
+        <br /><br />
     <asp:FileUpload id="FileUploadControl" runat="server" />
+        <br />
     <asp:Button runat="server" id="UploadButton" text="Upload" onclick="UploadButton_Click" />
     <br /><br />
     <asp:Label runat="server" id="StatusLabel" text="Upload status: " />
---
+++ LAWorksSite/LAWorksSite/UploadResume.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LAWorksSite
+{
+    public partial class UploadResume : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+
+            if (FileUploadControl.HasFile)
+            {
+                try
+                {
+
+                    HttpPostedFile file = FileUploadControl.PostedFile;
+                    string fileName = file.FileName;
+                    string filePath = Path.GetFullPath(fileName);
+                    string fileExtension = "";
+
+                    if (!string.IsNullOrEmpty(fileName))
+                        fileExtension = Path.GetExtension(fileName);
+
+                    StatusLabel.Text = fileExtension;
+
+                    if (FileUploadControl.PostedFile.ContentType.Equals("application/pdf") || 
+                        FileUploadControl.PostedFile.ContentType.Equals("application/msword") ||
+                        FileUploadControl.PostedFile.ContentType.Equals("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+                    {
+                        if (FileUploadControl.PostedFile.ContentLength < 102400)
+                        {
+                            
+                            StatusLabel.Text = "Upload status: File uploaded!";
+                        }
+                        else
+                            StatusLabel.Text = "Upload status: The file has to be less than 100 kb!";
+                    }
+                    else
+                        StatusLabel.Text = "Upload status: Only Word/PDF files are accepted!";
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+            }
+        }
+    }
+    
+}
