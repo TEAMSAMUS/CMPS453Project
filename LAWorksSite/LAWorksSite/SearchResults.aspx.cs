1c5f259b5d814dffd2e050b466a0d964275065f3
Sean Hungerford <sch4982@louisiana.edu>
2014-12-01 19:41:56 -0600

    New search results

--- LAWorksSite/LAWorksSite/SearchResults.aspx.cs
+++ LAWorksSite/LAWorksSite/SearchResults.aspx.cs
@@ -1,17 +1,32 @@
-﻿using System;
+﻿/*
+ * This program will be using GemBox.Document Free for creating Word Documents
+ *      in the code.
+ *      GemBox.Document Free can be found at http://www.gemboxsoftware.com/document/free-version
+ * 
+ */
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
+using System.Diagnostics;
+using GemBox.Document;
+
 
 namespace LAWorksSite
 {
+
+
     public partial class SearchResults : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+
     }
 }
