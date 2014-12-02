621cac4c1366e14b13cda0802b0312eaa4a070f8
ewoods2011 <edward.woods@rithmsgroup.com>
2014-12-01 21:03:53 -0600

    More Resume building!

--- LAWorksSite/LAWorksSite/Resume.aspx.designer.cs
+++ LAWorksSite/LAWorksSite/Resume.aspx.designer.cs
@@ -85,6 +85,24 @@ namespace LAWorksSite {
         protected global::System.Web.UI.WebControls.TextBox emailAddress;
         
         /// <summary>
+        /// objectiveRes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox objectiveRes;
+        
+        /// <summary>
+        /// abilitySum control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox abilitySum;
+        
+        /// <summary>
         /// education control.
         /// </summary>
         /// <remarks>
